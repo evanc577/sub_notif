6c65de92c3d8854dee861d85f84b8bb39cd564a4
Evan Chang <evanc577@gmail.com>
2023-06-11 21:10:43 -0400

    Change api backend

--- src/main.rs
+++ src/main.rs
@@ -1,174 +1,124 @@
-use anyhow::Result;
-use lazy_static::lazy_static;
-use reqwest::Url;
-use serde::Deserialize;
-use unqlite::{Transaction, UnQLite, KV};
+use std::io::ErrorKind;
+use std::time::Duration;
 
-const SEEN_DB: &str = "seen.udb";
+use anyhow::Result;
+use futures::stream::StreamExt;
+use reddit_api::structs::{Post, SubredditSort};
+use reddit_api::RedditClient;
+use serde::Deserialize;
+use time::format_description::well_known::Iso8601;
+use time::OffsetDateTime;
+use tokio::fs;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::time::MissedTickBehavior;
+
+const LAST_ID_FILE: &str = "last_seen.txt";
 const CONFIG_FILE: &str = "config.yaml";
 const REQ_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(10);
 const NUM_POSTS: usize = 50;
 
-lazy_static! {
-    static ref DB: unqlite::UnQLite = UnQLite::create(&SEEN_DB);
-    static ref CLIENT: reqwest::Client = reqwest::Client::builder()
-        .use_rustls_tls()
-        .timeout(REQ_TIMEOUT)
-        .build()
-        .unwrap();
-}
-
 #[derive(Deserialize, Debug)]
-struct CONFIG {
+struct Config {
     pushover_token: String,
     pushover_user: String,
     subreddit: String,
 }
 
-#[derive(Deserialize, Debug)]
-struct POResp {
-    status: i64,
-}
-
-#[derive(Deserialize, Debug)]
-struct PSResp {
-    data: Vec<Post>,
-}
-
-#[derive(Deserialize, Debug)]
-struct RedditResp {
-    data: RedditRespData,
-}
-
-#[derive(Deserialize, Debug)]
-struct RedditRespData {
-    children: Vec<RedditChildren>,
-}
-
-#[derive(Deserialize, Debug)]
-struct RedditChildren {
-    data: Post,
-}
-
-#[derive(Deserialize, Debug)]
-struct RedditChildrenData {
-    post: Post,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(untagged)]
-enum StringOrF64 {
-    String(String),
-    F64(f64),
-}
-
-#[derive(Deserialize, Debug)]
-struct Post {
-    title: String,
-    id: String,
-    created_utc: StringOrF64,
-}
-
 #[tokio::main]
 async fn main() {
     // parse config file
-    let config = &parse_config();
-
+    let config = &parse_config().await;
     let subreddit = &config.subreddit;
+    let reddit_client = RedditClient::new().unwrap();
+    let client = reqwest::Client::builder()
+        .use_rustls_tls()
+        .timeout(REQ_TIMEOUT)
+        .build()
+        .unwrap();
+    let mut last_id = last_id().await.unwrap();
 
+    let mut interval = tokio::time::interval(Duration::from_secs(10));
+    interval.set_missed_tick_behavior(MissedTickBehavior::Delay);
     loop {
-        let start = tokio::time::Instant::now();
+        interval.tick().await;
 
         // get recent posts
-        let r_fut = reddit(subreddit);
-        let ps_fut = pushshift(subreddit);
-        let (r_result, po_result) = tokio::join!(r_fut, ps_fut);
-        let r_posts = r_result.unwrap_or_else(|err| {
-            eprintln!("Reddit error: {}", err);
-            Vec::new()
-        });
-        let ps_posts = po_result.unwrap_or_else(|err| {
-            eprintln!("Pushshift error: {}", err);
-            Vec::new()
-        });
-
-        // concatenate posts
-        let posts = r_posts
-            .into_iter()
-            .chain(ps_posts.into_iter())
-            .collect::<Vec<_>>();
+        let posts = match reddit_posts(&reddit_client, subreddit, &last_id).await {
+            Ok(posts) => posts,
+            Err(_) => continue,
+        };
 
         // send notifications via pushover
         if !posts.is_empty() {
-            pushover(config, posts)
-                .await
-                .unwrap_or_else(|err| eprintln!("Pushover error: {}", err));
+            match pushover(config, &client, &posts).await {
+                Ok(_) => last_id = Some(posts[0].id.trim().to_owned()),
+                Err(e) => eprintln!("Pushover error: {}", e),
+            }
         }
-
-        // sleep for a while
-        tokio::time::sleep_until(start + tokio::time::Duration::from_secs(10)).await;
     }
 }
 
-fn parse_config() -> CONFIG {
-    let contents = std::fs::read_to_string(&CONFIG_FILE).unwrap_or_else(|_| {
-        eprintln!("Error: failed to open file {}", &CONFIG_FILE);
+async fn parse_config() -> Config {
+    let contents = fs::read_to_string(CONFIG_FILE).await.unwrap_or_else(|_| {
+        eprintln!("Error: failed to open file {}", CONFIG_FILE);
         panic!();
     });
 
     serde_yaml::from_str(&contents).unwrap_or_else(|err| {
-        eprintln!("Error parsing {}: {:?}", &CONFIG_FILE, err);
+        eprintln!("Error parsing {}: {:?}", CONFIG_FILE, err);
         panic!();
     })
 }
 
-async fn pushshift(subreddit: &str) -> Result<Vec<Post>> {
-    let pushshift_url = Url::parse_with_params(
-        "https://api.pushshift.io/reddit/submission/search",
-        &[
-            ("subreddit", subreddit),
-            ("size", NUM_POSTS.to_string().as_str()),
-        ],
-    )?;
+async fn last_id() -> Result<Option<String>> {
+    let mut f = match fs::File::open(LAST_ID_FILE).await {
+        Ok(f) => f,
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound {
+                return Ok(None);
+            }
+            return Err(e.into());
+        }
+    };
 
-    let resp = CLIENT.get(pushshift_url).send().await?;
-
-    let json: PSResp = serde_json::from_str(&resp.text().await?.as_str())?;
-
-    Ok(json.data)
+    let mut last_id = String::new();
+    f.read_to_string(&mut last_id).await?;
+    last_id = last_id.trim().to_owned();
+    Ok(Some(last_id))
 }
 
-async fn reddit(subreddit: &str) -> Result<Vec<Post>> {
-    let json = CLIENT
-        .get(&format!("https://api.reddit.com/r/{}/new.json", subreddit))
-        .query(&[("limit", NUM_POSTS.to_string())])
-        .header(reqwest::header::USER_AGENT, "sub notifier (by u/test2418945)")
-        .send()
-        .await?
-        .json::<RedditResp>()
-        .await?;
-
-    let temp = json.data.children.into_iter().map(|p| p.data).collect();
-    Ok(temp)
+async fn reddit_posts(
+    client: &RedditClient,
+    subreddit: &str,
+    last_id: &Option<String>,
+) -> Result<Vec<Post>> {
+    let query = client
+        .subreddit_posts_query()
+        .subreddit(subreddit)
+        .sort(SubredditSort::New)
+        .build();
+    let mut stream = query.execute().await.take(NUM_POSTS);
+    let mut posts = Vec::new();
+    while let Some(post) = stream.next().await {
+        let post = post?;
+        if let Some(last_id) = last_id {
+            if post.id.trim() == last_id {
+                break;
+            }
+        }
+        posts.push(post);
+    }
+    Ok(posts)
 }
 
-async fn pushover(config: &CONFIG, posts: Vec<Post>) -> Result<()> {
+async fn pushover(config: &Config, client: &reqwest::Client, posts: &[Post]) -> Result<()> {
     const POST_TIMEOUT_RETRY: i32 = 3;
 
     for post in posts.iter().rev() {
-        // check if post id has been seen
-        if DB.kv_contains(&post.id[..]) {
-            continue;
-        }
-
-        println!("Found new post: {}", post.id);
-
         for attempt in 0..POST_TIMEOUT_RETRY {
             // set parameters
-            let timestamp = match &post.created_utc {
-                StringOrF64::String(s) => s.clone(),
-                StringOrF64::F64(f) => format!("{}", f),
-            };
+            let timestamp =
+                OffsetDateTime::parse(&post.created_at, &Iso8601::DEFAULT)?.unix_timestamp();
             let decoded_title = htmlescape::decode_html(&post.title).unwrap_or(post.title.clone());
             let params = [
                 ("token", &config.pushover_token),
@@ -176,21 +126,21 @@ async fn pushover(config: &CONFIG, posts: Vec<Post>) -> Result<()> {
                 ("title", &format!("New post on r/{}", "dreamcatcher")),
                 ("message", &decoded_title),
                 ("url", &format!("https://redd.it/{}", post.id)),
-                ("timestamp", &timestamp),
+                ("timestamp", &timestamp.to_string()),
             ];
 
             // send POST
-            let resp = CLIENT
+            let resp = client
                 .post("https://api.pushover.net/1/messages.json")
                 .form(&params)
                 .send()
                 .await;
 
             // check if POST is ok
-            let resp_ok = match resp {
-                Ok(ok) => ok,
-                Err(err) => {
-                    if err.is_timeout() {
+            let resp = match resp {
+                Ok(r) => r,
+                Err(e) => {
+                    if e.is_timeout() {
                         // retry if timed out
                         eprintln!(
                             "POST {} to pushover timed out (attempt {} of {})",
@@ -201,31 +151,22 @@ async fn pushover(config: &CONFIG, posts: Vec<Post>) -> Result<()> {
                         continue;
                     } else {
                         // break if failed for other reason
-                        eprintln!("{:?}", err);
+                        eprintln!("{:?}", e);
                         break;
                     }
                 }
             };
 
-            // add post id to database if success
-            if resp_ok.status().is_success() {
-                match resp_ok.text().await {
-                    Err(err) => eprintln!("{:#?}", err), // could not parse resp body
-                    Ok(ok) => {
-                        let parsed_resp = serde_json::from_str::<POResp>(&ok[..]);
-                        match parsed_resp {
-                            Err(err) => eprintln!("{:#?}", err), // could not parse json
-                            Ok(ok) => {
-                                if ok.status == 1 {
-                                    // store post id in database
-                                    DB.kv_store(&post.id[..], "1")?;
-                                    DB.commit()?;
-                                    println!("Successfully pushed {}", post.id);
-                                }
-                            }
-                        };
+            // record last successful push
+            if resp.status().is_success() {
+                match resp.text().await {
+                    Err(e) => eprintln!("{:?}", e), // could not parse resp body
+                    Ok(_) => {
+                        let mut f = fs::File::create(LAST_ID_FILE).await.unwrap();
+                        f.write_all(post.id.as_bytes()).await.unwrap();
+                        println!("{}", &post.id);
                     }
-                };
+                }
             }
 
             break;
